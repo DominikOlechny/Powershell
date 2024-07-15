96a72f1bf00f1d99c7e3ec7fb32edc3765fe0537
DominikOlechny <116365805+DominikOlechny@users.noreply.github.com>
2024-07-15 13:55:52 +0200

    Grupa dystrybucyjna
    Skrypt na dodawanie userów do grupy dystrybucyjnej z listy w pliku txt.

---
+++ Dodawanie_DO_Dystrybucyjnej_Z_Pliku_TXT.ps1
@@ -0,0 +1,14 @@
+﻿#początek skryptu
+Import-Module ExchangeOnlineManagement
+Connect-ExchangeOnline -UserPrincipalName XXXX@xxx.xx -ShowProgress $true #tutaj daj poprawnego admina tenanta
+# Wczytaj listę użytkowników
+$users = Get-Content "sciezka_do_pliku.txt" #tutaj zmień
+
+# Nazwa grupy dystrybucyjnej
+$group = "XXXX@xxx.xx" #tutaj zmien grupe dystrybucyjna na swoja 
+
+# Dodaj użytkowników do grupy dystrybucyjnej
+foreach ($user in $users) {
+    Add-DistributionGroupMember -Identity $group -Member $user
+}
+##koniec skryptu
